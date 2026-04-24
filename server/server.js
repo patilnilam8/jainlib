da45d3c49e19853fb82e293fec264578dc35d8f3
unknown <patil.nilam8@gmail.com>
2026-04-25 02:49:03 +0530

    vercel

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-frontend-url.vercel.app"
+    "https://jainlib.vercel.app/"
   ],
   credentials: true
 }));
