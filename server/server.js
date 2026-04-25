15d2af9c2f5eb8144a5eee16d4296f3db9934b15
unknown <patil.nilam8@gmail.com>
2026-04-25 11:10:14 +0530

    netilfy

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://jainlib.vercel.app",
+    "https://digajain.netlify.app",
     "https://digambarjain.in",
     "https://www.digambarjain.in"
   ],
