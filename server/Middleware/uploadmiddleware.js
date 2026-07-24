65aca17f2b47e19c6bac4379fe316c90cd3d0d7e
unknown <patil.nilam8@gmail.com>
2026-07-25 01:04:44 +0530

    cloud

--- server/Middleware/uploadmiddleware.js
+++ server/Middleware/uploadmiddleware.js
@@ -1,6 +1,7 @@
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("cloudinary").v2;
+const path = require("path");
 
 // Config
 cloudinary.config({
@@ -12,33 +13,43 @@ cloudinary.config({
 // Storage
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: async (req, file) => {
-    let folder = "others";
-    let resource_type = "auto";
+ params: async (req, file) => {
+  let folder = "others";
+  let resource_type = "auto";
 
-    if (file.mimetype === "application/pdf") {
-      folder = "granth_pdfs";
-      resource_type = "raw"; // IMPORTANT for PDF
-    } else if (file.mimetype.startsWith("image")) {
-      if (file.fieldname === "coverPhoto") {
-        folder = "granth_covers";
-      } else {
-        folder = "granth_images";
-      }
+  if (file.mimetype === "application/pdf") {
+    folder = "granth_pdfs";
+    resource_type = "raw";
+  } else if (file.mimetype.startsWith("image")) {
+    if (file.fieldname === "coverPhoto") {
+      folder = "granth_covers";
+    } else {
+      folder = "granth_images";
     }
-
-    const name = req.body.name || "unnamed";
-    const englishName = req.body.englishName || "unnamed";
-
-    const safeName = `${name}_${englishName}`
-      .replace(/[^a-zA-Z0-9_\-–—\s\u0900-\u097F]/g, "_");
-
-    return {
-      folder,
-      public_id: safeName,
-      resource_type
-    };
   }
+
+  const name = req.body.name || "unnamed";
+  const englishName = req.body.englishName || "unnamed";
+
+  const safeName = `${name}_${englishName}`.replace(
+    /[^a-zA-Z0-9_\-\s\u0900-\u097F]/g,
+    "_"
+  );
+
+  const ext = path.extname(file.originalname);
+
+  let publicId = safeName;
+
+  if (file.mimetype === "application/pdf") {
+    publicId = `${safeName}${ext}`;
+  }
+
+  return {
+    folder,
+    public_id: publicId,
+    resource_type
+  };
+}
 });
 
 const fileFilter = (req, file, cb) => {
